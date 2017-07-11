bdf95644aaacded1d1da9e7a5638d3a169e4b67a
kc657 <kvn.0218@gmail.com>
2017-07-11 09:57:39 -0700

    Formatting and deleting obsolete lines

--- scripts/app.js
+++ scripts/app.js
@@ -1,30 +1,20 @@
 console.log('sanity check')
 
-// $(window).onkeydown(function (key) {
-//     //use e.which
-//     var keyCode = key.which;
-//     console.log(key, keyCode, key.which)
-//     if (keyCode == 88) {
-//         console.log("You pressed W!");
-//         //alert("You pressed W!");
-//     }
-// })
+// listener for "z" keypress
+$(document).keydown(function (pressZ) {
+  if (pressZ.keyCode == 90) { $('body').append('<p>z detected!</p>') }
+})
 
+// listener for "b" keypress
+$(document).keydown(function (pressB) {
+  if (pressB.keyCode == 66) {
+    $('body').append('<p>b detected!</p>')
+  }
+})
 
-//listener for "z" keypress
-$(document).keydown(function(e){
-if (e.keyCode==90)
-    $("body").append("<p>z detected!</p>");
-});
-
-//listener for "b" keypress
-$(document).keydown(function(e){
-if (e.keyCode==66)
-    $("body").append("<p>b detected!</p>");
-});
-
-//listener for "b" keypress
-$(document).keydown(function(e){
-if (e.keyCode==39)
-    $("body").append("<p>left arrow detected!</p>");
-});
+// listener for "b" keypress
+$(document).keydown(function (pressRight) {
+  if (pressRight.keyCode == 39) {
+    $('body').append('<p>Right arrow detected!</p>')
+  }
+})
