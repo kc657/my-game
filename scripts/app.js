12d04df4214b40978c3c54ac09ed5cf36b077734
kc657 <kvn.0218@gmail.com>
2017-07-11 16:14:50 -0700

    Pieces stop at end, move according to page width

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,7 @@ console.log('sanity check')
 
 // listener for "z" keypress
 
-$(document).ready(function(){
+$(document).ready(function () {
   var firstPlace = ''
   var secondPlace = ''
   var racerOnePosition = 0
@@ -11,20 +11,36 @@ $(document).ready(function(){
   var racerOne = $('.racerOne')
   var racerTwo = $('.racerTwo')
   var racerThree = $('.racerThree')
+  var width = $(window).width()
+  var cssWidth = $('.track').width()
 
-  $(document).keydown(function pressZ (userInput){
-    if(userInput.keyCode === 90){
-      racerOnePosition += 25;
-      console.log("racer 1 is ", racerOne)
-      racerOne.css({left: racerOnePosition});
-    } else if (userInput.keyCode === 78){
-      racerTwoPosition += 25;
-      console.log("racer 1 is ", racerTwo)
-      racerTwo.css({left: racerTwoPosition});
-    } else if(userInput.keyCode === 39){
-      racerThreePosition += 25;
-      console.log("racer 1 is ", racerThree)
-      racerThree.css({left: racerThreePosition});
+  console.log(cssWidth)
+
+  $(document).keydown(function (userInput) {
+    if (userInput.keyCode === 90) {
+      if (racerOnePosition <= cssWidth - (width - cssWidth)) {
+        racerOnePosition += cssWidth / 25
+        console.log('racer 1 is ', racerOne)
+        racerOne.css({left: racerOnePosition})
+      } else {
+        console.log('Game Over')
+      }
+    } else if (userInput.keyCode === 78) {
+      if (racerTwoPosition <= cssWidth - (width - cssWidth)) {
+        racerTwoPosition += cssWidth / 25
+        console.log('racer 1 is ', racerTwo)
+        racerTwo.css({left: racerTwoPosition})
+      } else {
+        console.log('Game Over')
+      }
+    } else if (userInput.keyCode === 39) {
+      if (racerThreePosition <= cssWidth - (width - cssWidth)) {
+        racerThreePosition += cssWidth / 25
+        console.log('racer 1 is ', racerThree)
+        racerThree.css({left: racerThreePosition})
+      } else {
+        console.log('Game Over')
+      }
     }
   })
-});
+})
