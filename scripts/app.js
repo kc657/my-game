50f2364ba8607978c07b7d2b23fc349177abdf30
kc657 <kvn.0218@gmail.com>
2017-07-11 23:03:29 -0700

    Created better transition across finish line in js

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,8 @@
 console.log('sanity check')
 
-// listener for "z" keypress
-
 $(document).ready(function () {
+
+  //defining variables
   var firstPlace = null
   var secondPlace = null
   var gameOver = false
@@ -12,67 +12,89 @@ $(document).ready(function () {
   var racerOne = $('.racerOne')
   var racerTwo = $('.racerTwo')
   var racerThree = $('.racerThree')
+  var chaser = $('.chaser')
+  var chaserPosition = 0
   var width = $(window).width()
   var cssWidth = $('.track').width()
 
+  function autoChasing(){
+    for (i=chaserPosition; i < cssWidth; i++) {
+        chaser.css({left: chaserPosition})
+      }
+    }
+
+    autoChasing()
+
   $(document).keydown(function (userInput) {
+
+    //user input function
     if (userInput.keyCode === 90) {
       if (racerOnePosition <= cssWidth - (width - cssWidth)) {
         racerOnePosition += cssWidth / 25
-        console.log('racer 1 is ', racerOne)
         racerOne.css({left: racerOnePosition})
       } else {
         if (firstPlace === null) {
+          racerOnePosition += 25
+          racerOne.css({left: racerOnePosition})
           firstPlace = 'Player One'
           $('.results').append('<p>Player One Finished First and Lost!<p>')
           racerOne.off
         } else if (secondPlace === null && firstPlace !== 'Player One') {
+          racerOnePosition += 25
+          racerOne.css({left: racerOnePosition})
           secondPlace = 'Player One'
-          $('.results').append('<p>Player One Finished Second and Won!<p>')
+          $('.results').append('<p><span id="winner">Player One Finished Second and Won!</span><p>')
         } else if (firstPlace && secondPlace !== null) {
           gameOver = true
-          $('.results').append(`<p>Game has ended</p>`)
+          $('.results').append(`<p>Game has ended. Press restart button to play again!</p>`)
         }
       }
     } else if (userInput.keyCode === 78) {
       if (racerTwoPosition <= cssWidth - (width - cssWidth)) {
         racerTwoPosition += cssWidth / 25
-        console.log('racer 1 is ', racerTwo)
         racerTwo.css({left: racerTwoPosition})
       } else {
         if (firstPlace === null) {
+          racerTwoPosition += 25
+          racerTwo.css({left: racerTwoPosition})
           firstPlace = 'Player Two'
           $('.results').append('<p>Player One Finished First and Lost!<p>')
           racerTwo.off
         } else if (secondPlace === null && firstPlace !== 'Player Two') {
+          racerTwoPosition += 25
+          racerTwo.css({left: racerTwoPosition})
           secondPlace = 'Player Two'
-          $('.results').append('<p>Player Two Finished Second and Won!<p>')
+          $('.results').append('<p><span id="winner">Player Two Finished Second and Won!</span><p>')
         } else if (firstPlace && secondPlace !== null) {
           gameOver = true
-          $('.results').append(`<p>Game has ended</p>`)
+          $('.results').append(`<p>Game has ended. Press restart button to play again!</p>`)
         }
       }
     } else if (userInput.keyCode === 39) {
       if (racerThreePosition <= cssWidth - (width - cssWidth)) {
         racerThreePosition += cssWidth / 25
-        console.log('racer 1 is ', racerThree)
         racerThree.css({left: racerThreePosition})
       } else {
         if (firstPlace === null) {
+          racerThreePosition += 25
+          racerThree.css({left: racerThreePosition})
           firstPlace = 'Player Three'
           $('.results').append('<p>Player Three Finished First and Lost!<p>')
           racerThree.off
         } else if (secondPlace === null && firstPlace !== 'Player Three') {
+          racerThreePosition += 25
+          racerThree.css({left: racerThreePosition})
           secondPlace = 'Player Three'
-          $('.results').append('<p>Player Three Finished Second and Won!<p>')
+          $('.results').append('<p><span id="winner">Player Three Finished Second and Won!</span><p>')
         } else if (firstPlace && secondPlace !== null) {
           gameOver = true
-          $('.results').append(`<p>Game has ended</p>`)
+          $('.results').append(`<p>Game has ended. Press restart button to play again!</p>`)
         }
       }
     }
   })
 })
+
 // function (playerName) {
 // if (firstPlace === null) {
 //   firstPlace = 'Player One'
